626e907f722780a99f6df278c57e1a31abbbbabe
Lassi Säike <lassisaike11@gmail.com>
2019-09-05 12:12:30 +0300

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,34 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import bpy
+from .ValveFileSystem import valve
+
+bl_info = {
+    "name": "import-vmt",
+    "author": "lasa01",
+    "description": "Import Source Engine VMT files as materials",
+    "blender": (2, 80, 0),
+    "version": (0, 0, 1),
+    "location": "",
+    "warning": "",
+    "category": "Import-Export"
+}
+
+
+def register():
+    ...
+
+
+def unregister():
+    ...
